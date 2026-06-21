cf76d284758fed940287ae3d605a9018a47221da
Bb-02 <1598270553@qq.com>
2026-06-21 23:15:18 +0800

    创建Mapper层

--- smartam-tenant/src/main/java/com/chengmaomao/smartam/SmartAmApplication.java
+++ smartam-tenant/src/main/java/com/chengmaomao/smartam/SmartAmApplication.java
@@ -1,9 +1,11 @@
 package com.chengmaomao.smartam;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chengmaomao.smartam.tenant.mapper")
 public class SmartAmApplication {
 
     public static void main(String[] args) {
